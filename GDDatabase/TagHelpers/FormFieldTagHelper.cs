c803a0a817f8a5e2dc351b8b8c063b4c38ffc36c
aidan-h <aidanhammond2003@gmail.com>
2024-04-02 16:48:57 -0500

    Forgot to add tag helpers

---
+++ GDDatabase/TagHelpers/FormFieldTagHelper.cs
@@ -0,0 +1,69 @@
+﻿using Game_Design_DB.ViewModels;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Linq;
+using System.Text;
+
+namespace Game_Design_DB.TagHelpers
+{
+        [HtmlTargetElement("form-field")]
+        public class FormFieldTagHelper : TagHelper
+        {
+            [HtmlAttributeName("asp-for")]
+            public ModelExpression For { get; set; }
+
+            private readonly IHtmlGenerator _generator;
+
+            [ViewContext]
+            public ViewContext ViewContext { get; set; }
+
+            public FormFieldTagHelper(IHtmlGenerator generator)
+            {
+                _generator = generator;
+            }
+
+            public override void Process(TagHelperContext context, TagHelperOutput output)
+            {
+                using (var writer = new StringWriter())
+                {
+                    writer.Write(@"<div class=""form-group"">");
+
+                    var label = _generator.GenerateLabel(
+                                    ViewContext,
+                                    For.ModelExplorer,
+                                    For.Name, null,
+                                    new { @class = "control-label" });
+
+                    label.WriteTo(writer, NullHtmlEncoder.Default);
+
+                    var textArea = _generator.GenerateTextBox(ViewContext,
+                                        For.ModelExplorer,
+                                        For.Name,
+                                        For.Model,
+                                        null,
+                                        new { @class = "form-control" });
+
+                    textArea.WriteTo(writer, NullHtmlEncoder.Default);
+
+                    var validationMsg = _generator.GenerateValidationMessage(
+                                            ViewContext,
+                                            For.ModelExplorer,
+                                            For.Name,
+                                            null,
+                                            ViewContext.ValidationMessageElement,
+                                            new { @class = "text-danger" });
+
+                    validationMsg.WriteTo(writer, NullHtmlEncoder.Default);
+
+                    writer.Write(@"</div>");
+
+                    output.Content.SetHtmlContent(writer.ToString());
+
+                }
+
+            }
+        }
+    }
---
+++ GDDatabase/TagHelpers/FormManyFieldTagHelper.cs
@@ -0,0 +1,54 @@
+﻿using Game_Design_DB.ViewModels;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Linq;
+using System.Text;
+
+namespace Game_Design_DB.TagHelpers
+{
+    [HtmlTargetElement("form-many-field")]
+    public class FormManyFieldTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-for")]
+        public ModelExpression For { get; set; }
+
+        [HtmlAttributeName("field")]
+        public string Field { get; set; }
+
+
+        private readonly IHtmlGenerator _generator;
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public FormManyFieldTagHelper(IHtmlGenerator generator)
+        {
+            _generator = generator;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            using (var writer = new StringWriter())
+            {
+                writer.Write(@"<div class=""form-group""><div class=""col-md-offset-2 col-md-10"">");
+
+                var n = For.Model as ICollection<PersonAssigned>;
+                foreach (var author in n)
+                {
+                    writer.Write(@"<div><input type=""checkbox"" name=" + Field + @""" value=" + author.ID + @""" ");
+                    if (author.Assigned)
+                    {
+                        writer.Write(@"checked=""checked""");
+                    }
+                    writer.Write(@"/> " + author.Name + @"</div>");
+                }
+                writer.Write(@"</div></div>");
+                output.Content.SetHtmlContent(writer.ToString());
+            }
+
+        }
+    }
+}
